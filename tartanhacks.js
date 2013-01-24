f6586da2909cacd6dbb778329b4df1cb35e3e9be
Alexey Komissarouk <AlexeyMK@gmail.com>
2013-01-23 18:27:31 -0800

    refactored to look nicer side by side in screen, added slides

--- tartanhacks.js
+++ tartanhacks.js
@@ -1,33 +1,17 @@
-Hackathons = new Meteor.Collection('hackathons');
+Languages =
+  new Meteor.Collection('languages');
 
 if (Meteor.isClient) {
-  Template.hackathons.hackathons = function () {
-    return Hackathons.find({}, {sort: {"votes": -1}});
+  Template.language_voter.list_languages =
+    function () {
+      return Languages.find({},
+              {sort: {"votes": -1}});
   };
 
-  Template.hackathon.events({
+  Template.language.events({
     'click input' : function () {
-      Hackathons.update({name: this.name},
-                        {$inc: {votes: 1}});
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    if (!Hackathons.findOne()) {
-      Hackathons.insert({
-        name: "PennApps",
-        votes: 1,
-      });
-      Hackathons.insert({
-        name: "Tartan Hacks",
-        votes: 0,
-      });
-      Hackathons.insert({
-        name: "HackNY",
-        votes: 0,
-      });
+      Languages.update({name: this.name},
+                       {$inc: {votes: 1}});
     }
   });
 }
